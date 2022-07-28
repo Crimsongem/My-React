c64726c29d51fe8790fcfb1a4d1df4462e49fe4d
Crimsongem <jaffy038@gmail.com>
2022-07-28 11:50:20 +0700

    ADD File

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+
+
+function Page_home(){
+    return(
+        <div className="container">
+            <div className="Home">
+                
+                <p className="text-center">
+                    <h1>หน้าแรก</h1>
+                    จัดทำโดย. นายเมธสิทธิ์ ชวลิต
+                </p>
+            </div>
+        </div>
+    );
+}
+
+
+export default Page_home;
